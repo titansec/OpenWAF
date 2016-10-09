ed007ea7f612c810225832fcd7a15a18db2817a5
Miracle <290557551@qq.com>
2016-10-09 17:38:15 +0800

    Update twaf_init.lua

--- app/twaf_init.lua
+++ app/twaf_init.lua
@@ -5,15 +5,14 @@ require "resty.core"
 -- construct a new object - twaf_config
 local twaf_config_m = require "lib.twaf.twaf_conf"
 twaf_config = twaf_config_m:new()
-twaf_config:load_default_config("/usr/local/openresty/lualib/openwaf/conf")
-twaf_config:load_access_rule("/usr/local/openresty/lualib/openwaf/conf")
-twaf_config:load_policy_config("/usr/local/openresty/lualib/openwaf/conf",
-                               {twaf_global_conf = 1, twaf_policy_conf = 1})
+twaf_config:load_default_config("/opt/OpenWAF/conf/twaf_default_conf.json")
+twaf_config:load_access_rule("/opt/OpenWAF/conf")
+twaf_config:load_policy_config("/opt/OpenWAF/conf", {twaf_policy_conf = 1})
 twaf_config:load_rules()
 
 -- GeoIP 
-twaf_config:load_geoip_country_ipv4("/usr/local/openresty/lualib/openwaf/lib/twaf/inc/knowledge_db/geo_country/GeoIP.dat")
-twaf_config:load_geoip_country_ipv6("/usr/local/openresty/lualib/openwaf/lib/twaf/inc/knowledge_db/geo_country/GeoIPv6.dat")
+twaf_config:load_geoip_country_ipv4("/opt/OpenWAF/lib/twaf/inc/knowledge_db/geo_country/GeoIP.dat")
+twaf_config:load_geoip_country_ipv6("/opt/OpenWAF/lib/twaf/inc/knowledge_db/geo_country/GeoIPv6.dat")
 
 local twaf_reqstat_m = require "lib.twaf.twaf_reqstat"
 twaf_reqstat = twaf_reqstat_m:new(twaf_config.twaf_default_conf.twaf_reqstat, twaf_config.twaf_policy.policy_uuids)
