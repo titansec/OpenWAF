10650a38a3bef93db10531705036dc2d6ef702a4
Miracle <290557551@qq.com>
2018-01-19 14:15:46 +0800

    Upload API: dynamic_config_back

--- lib/twaf/twaf_api/dynamic_config.lua
+++ lib/twaf/twaf_api/dynamic_config.lua
@@ -3,14 +3,17 @@
 -- Copyright (C) OpenWAF
 
 local _M = {
-    _VERSION = "0.0.2"
+    _VERSION = "0.0.3"
 }
 
 local twaf_func = require "lib.twaf.inc.twaf_func"
+local cjson     = require "cjson.safe"
+local back_path = "/opt/OpenWAF/conf/dynamic_conf_back.json"
 
 _M.api = {}
 _M.help = {}
 _M.api.dynamic_config = {}
+_M.api.dynamic_config_back = {}
 
 -- get dynamic config e.g: GET /api/dynamic_config
 _M.api.dynamic_config.get  = function(_twaf, log, u)
@@ -19,6 +22,16 @@ _M.api.dynamic_config.get  = function(_twaf, log, u)
     
     result.twaf_policy      = config.twaf_policy
     result.twaf_access_rule = config.twaf_access_rule
+    
+    local f, err = io.open(back_path, "w+")
+    if not f then 
+        log.success = 0
+        log.reason  = err
+        return
+    end
+    
+    f:write(twaf_func:table_to_string(result))
+    f:close()
     
     log.result = result
     
@@ -57,9 +70,56 @@ _M.api.dynamic_config.post = function(_twaf, log, u)
     return
 end
 
+-- post dynamic config back file e.g: POST /api/dynamic_config_back
+_M.api.dynamic_config_back.post = function(_twaf, log, u)
+
+    local f, err = io.open(back_path)
+    if not f then 
+        log.success = 0
+        log.reason  = err
+        return
+    end
+    
+    local data = f:read("*a")
+    if not data then 
+        f:close()
+        log.success = 0
+        log.reason  = "f:read is nil"
+        return
+    end
+    
+    f:close()
+    
+    local res, config = pcall(cjson.decode, data)
+    if not res or type(config) ~= "table" then
+        log.success = 0
+        log.reason  = "json expected, got " .. (data or "nil")
+        return
+    end
+    
+    if config.twaf_access_rule == nil or
+       config.twaf_policy      == nil then
+        log.success = 0
+        log.reason  = "Wrong dynamic config"
+        return
+    end
+    
+    local p_conf = twaf_func:syn_config_process(_twaf, config)
+    
+    _twaf.config.twaf_access_rule = p_conf.twaf_access_rule
+    _twaf.config.twaf_policy      = p_conf.twaf_policy
+    
+    log.result = p_conf
+    return
+end
+
 _M.help.dynamic_config = {
     "GET /api/dynamic_config",
     "POST /api/dynamic_config"
+}
+
+_M.help.dynamic_config_back = {
+    "POST /api/dynamic_config_back"
 }
     
 return _M
